e535176d68b91bf44253412dec385f234fe1e82d
guygastineau <strings.stringsandstrings@gmail.com>
2020-06-09 14:57:48 -0400

    got all the GI dependencies figured out!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE
+    OverloadedLabels
+  , OverloadedLists
+  , OverloadedStrings
+  #-}
+
 module Main where
 
-import Lib
+--import Lib
+
+import GI.Gtk (Label(..), Window(..))
+import GI.Gtk.Declarative
+import GI.Gtk.Declarative.App.Simple
 
 main :: IO ()
 main = putStrLn "Hello"
